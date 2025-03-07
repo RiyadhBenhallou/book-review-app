bbb375c0d688221ecf4c436d929a8dce1350abe5
RiyadhBenhallou <riadhriadh2004@gmail.com>
2025-03-07 16:10:28 +0100

    Refactor Navbar component, remove authentication logic, and integrate AddBookDialog

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,12 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { BookHeart } from "lucide-react";
 import Link from "next/link";
-import { loginAction } from "../actions";
-import { getMember, getServerClient } from "@/lib/wix";
+import AddBookDialog from "../books/_components/add-book-dialog";
 
 export default async function Navbar() {
-  const member = await getMember();
-  const isLoggedIn = (await getServerClient()).auth.loggedIn();
   return (
     <div className="bg-slate-200">
       <div className="container mx-auto px-4 py-4 max-w-6xl flex items-center justify-between">
@@ -17,19 +14,10 @@ export default async function Navbar() {
           </div>
         </Link>
         <div className="flex items-center gap-4">
-          <Link href={"/books"}>Books</Link>
-          {isLoggedIn ? (
-            <div className="flex items-center gap-2">
-              <p className="text-sm">Hello, {member?.nickname}</p>
-              {/* <form action={}> */}
-              <Button variant="outline">Logout</Button>
-              {/* </form> */}
-            </div>
-          ) : (
-            <form action={loginAction}>
-              <Button variant="outline">Login</Button>
-            </form>
-          )}
+          <Button variant="secondary" asChild>
+            <Link href={"/books"}>Books</Link>
+          </Button>
+          <AddBookDialog />
         </div>
       </div>
     </div>
--- lib/wix.ts
+++ lib/wix.ts
@@ -1,7 +1,5 @@
 import { items } from "@wix/data";
 import { createClient, OAuthStrategy } from "@wix/sdk";
-import { members, authorization } from "@wix/members";
-import { cookies } from "next/headers";
 
 export const client = createClient({
   modules: { items },
@@ -9,33 +7,3 @@ export const client = createClient({
     clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID as string,
   }),
 });
-
-export async function getServerClient() {
-  const session = (await cookies()).get("session")?.value || "null";
-  return createClient({
-    modules: { items, members, authorization },
-    auth: OAuthStrategy({
-      clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
-      tokens: JSON.parse(session),
-    }),
-  });
-}
-
-export async function getMember() {
-  const client = await getServerClient();
-
-  if (!client.auth.loggedIn()) {
-    return undefined;
-  }
-
-  const { member } = await client.members.getCurrentMember();
-
-  return member
-    ? {
-        id: member._id,
-        loginEmail: member.loginEmail,
-        nickname: member.profile?.nickname,
-        slug: member.profile?.slug,
-      }
-    : undefined;
-}
